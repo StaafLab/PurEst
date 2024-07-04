de7cba1d2bf223c7cfb45bcc9311317f07937d28
Iñaki Sasiain <inakisasiainca@gmail.com>
2024-07-04 16:30:48 +0200

    Adding exmple files

---
+++ PurEst/R/example_betas_reference.R
@@ -0,0 +1,8 @@
+#' @title example_betas_reference
+#'
+#' @description A marix containing the uncorrected beta values of the 5.000 most variable CpGs from the TCGA-BRCA dataset. This matrix is intended to be used as example reference data for the PurEst package.
+#'
+#' @format A matrix with 5000 rows (CpGs) and 126 columns (samples).
+#'
+#' @source <https://github.com/StaafLab/PurEst>
+"example_betas_reference"
